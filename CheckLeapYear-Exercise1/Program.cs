2b028dffd132a9fb14fe7c3404a7cf81d5d3b679
ElinaBM <36169512+ElinaBM@users.noreply.github.com>
2018-12-04 14:44:29 +0200

    Add files via upload

---
+++ CheckLeapYear-Exercise1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CheckLeapYear_ConditionalStatements_Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int year;
+
+            Console.WriteLine("Please enter a year!");
+
+            while (int.TryParse(Console.ReadLine(), out year) == false)
+            {
+                Console.WriteLine("Please enter year as a number!");
+            }
+
+
+            if (DateTime.IsLeapYear(year) == true)
+            {
+                Console.WriteLine("This is a leap year!");
+            }
+
+            else
+            {
+                Console.WriteLine("This is not a leap year!");
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
